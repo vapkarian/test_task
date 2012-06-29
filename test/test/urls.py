742c0842632ec04901b4431aa73a9779d389bd09
vapkarian <victor.apkarian@gmail.com>
2012-06-29 13:53:36 +0300

    Tasks 1 and 2 are done

--- test/test/urls.py
+++ test/test/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
+                       (r'^accounts/login/$',  login),
+                       (r'^accounts/logout/$', logout),
                        url(r'^$','test.views.index'),
 )
