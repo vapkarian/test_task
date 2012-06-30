80e1cef2618008256808ed4460ab4282b199f69f
vapkarian <victor.apkarian@gmail.com>
2012-07-01 01:50:39 +0300

    Task 6 is done

--- test/myprofile/forms.py
+++ test/myprofile/forms.py
@@ -1,9 +1,10 @@
 from django import forms
+from django.contrib.admin.widgets import AdminDateWidget
 
 class ChangeForm(forms.Form):
     first_name = forms.CharField(max_length=35)
     last_name = forms.CharField(max_length=50)
-    birthday = forms.DateField(required=False)
+    birthday = forms.DateField(required=False,widget=AdminDateWidget())
     biography = forms.CharField(max_length=3000,
                                 required=False,widget=forms.Textarea)
     contacts = forms.CharField(max_length=500,
--- test/test/urls.py
+++ test/test/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
@@ -5,8 +6,15 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^admin/',include(admin.site.urls)),
+                       url(r'^my_admin/jsi18n',
+                           'django.views.i18n.javascript_catalog'),
+                       url(r'^my_admin/jsi18n',
+                           'django.views.i18n.javascript_catalog'),
+                       url(r'^accounts/edit/$','myprofile.views.change'),
                        url(r'^accounts/login/$',login),
                        url(r'^accounts/logout/$',logout),
-                       url(r'^accounts/edit/$','myprofile.views.change'),
+                       url(r'^static/(?P<path>.*)$',
+                           'django.views.static.serve',
+                           {'document_root': settings.STATIC_ROOT,}),
                        url(r'^$','test.views.index'),
 )
